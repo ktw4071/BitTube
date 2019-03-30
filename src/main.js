93bb582a531735e0593be285b19f343dce4ffd29
TaewooKim <taewookimit@gmail.com>
2019-03-30 13:57:27 -0400

    first commit for BIT NET Final Proejct, BitTube

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
+import VueXgplayer from 'vue-xgplayer'
+
+Vue.use(VueXgplayer, {
+  // globalOptions
+  enterLogo: {
+    url: 'http://192.168.2.76:5000/IMG_20180710_200333_950.jpg',
+    width: 100,
+    height: 40
+  },
+  playsinline: true
+})
 
 Vue.config.productionTip = false
 
